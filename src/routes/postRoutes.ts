e4b4f999132c7ff186b5e8be59c999f459012032
Shrayansh Jain <jainshrayansh1998@gmail.com>
2024-01-31 18:59:45 +0530

    created all post routes

---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../controllers/postController';
+
+const router = Router();
+
+router.get('/posts', getAllPosts);
+router.get('/posts/:id', getPostById);
+router.post('/posts', createPost);
+router.put('/posts/:id', updatePost);
+router.delete('/posts/:id', deletePost);
+
+export default router;
